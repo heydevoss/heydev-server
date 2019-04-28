afd2e65a142e228ce195e83b262715e1c4c3fc62
LukeHxH <lucasmed812@gmail.com>
2019-04-28 17:17:38 -0300

    Refactor of application GraphQL Schema

--- src/index.js
+++ src/index.js
@@ -4,15 +4,13 @@ import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 
 import schema from './schema';
-import resolvers from './resolvers';
 import models from './models';
 
 const app = express();
 app.use(cors());
 
 const server = new ApolloServer({
-  typeDefs: schema,
-  resolvers,
+  schema: schema,
   context: {
     models,
     me: models.users[1]
@@ -22,5 +20,5 @@ const server = new ApolloServer({
 server.applyMiddleware({ app, path: '/graphql' });
 
 app.listen({ port: 5000 }, () => {
-  console.log('Apollo Server on http://localhost:5000/graphql');
+  console.log('Apollo Server listening on http://localhost:5000/graphql');
 });
--- src/schema/index.js
+++ src/schema/index.js
@@ -1,14 +1,44 @@
-import { gql } from 'apollo-server-express';
+import {
+  GraphQLObjectType,
+  GraphQLID,
+  GraphQLString,
+  GraphQLList,
+  GraphQLSchema
+} from 'graphql';
 
-export default gql`
-  type Query {
-    users: [User!]
-    user(id: ID!): User
-    me: User
-  }
+import resolvers from '../resolvers';
 
-  type User {
-    id: ID!
-    username: String!
+console.log(resolvers.Query.users);
+
+const UserType = new GraphQLObjectType({
+  name: 'User',
+  fields: () => ({
+    id: { type: GraphQLID },
+    username: { type: GraphQLString }
+  })
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'Query',
+  fields: {
+    users: {
+      type: new GraphQLList(UserType),
+      resolve: resolvers.Query.users
+    },
+
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLID } },
+      resolve: resolvers.Query.user
+    },
+
+    me: {
+      type: UserType,
+      resolve: resolvers.Query.me
+    }
   }
-`;
+});
+
+export default new GraphQLSchema({
+  query: RootQuery
+});
