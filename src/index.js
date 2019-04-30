c1da30c36582b05f9a22987bd409c55af380bedc
Jadson Luan <jadsonluan1@gmail.com>
2019-04-30 16:10:03 -0300

    Implement authentication route

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
+import cookieParser from 'cookie-parser';
+
 import { ApolloServer } from 'apollo-server-express';
+
+import authRouter from './routes/githubAuth';
 
 import schema from './schema';
 import models from './models';
 
 const app = express();
 app.use(cors());
+app.use(cookieParser());
+app.use('/auth', authRouter);
 
 const server = new ApolloServer({
   schema: schema,
