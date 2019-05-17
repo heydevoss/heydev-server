d98cb6c4b48e5ba625fbf2dd4a9d18bb3b85a332
LukeHxH <lucasmed812@gmail.com>
2019-05-17 16:30:24 -0300

    Add resolvers for commits in organization

--- src/schema/organization/resolvers.js
+++ src/schema/organization/resolvers.js
@@ -1,8 +1,48 @@
 import fetchData from '../../github/dataFetcher';
 import githubQueries from '../../github/queries';
 
+const sumCommits = (commits) => {
+  let sum = 0;
+
+  commits.forEach(commit => {
+    if (commit.object)
+      sum += commit.object.history.totalCount;
+  });
+
+  return sum;
+}
+
 export default {
   Query: {
+    commits: async (parent, args, { token }) => {
+      const pagination = 80;
+
+      let body = githubQueries.totalCommitsOrganization(parent.login, pagination);
+      let data = await fetchData(body, token);
+
+      let hasNext = true;
+
+      let totalCommits = 0;
+
+      do {
+        let repositories = data.data.repositoryOwner.repositories;
+
+        const commits = repositories.nodes;
+        const pageInfo = repositories.pageInfo;
+        const cursor = pageInfo.endCursor;
+
+        hasNext = pageInfo.hasNextPage;
+
+        totalCommits += sumCommits(commits);
+
+        if (hasNext) {
+          body = githubQueries.totalCommitsOrganizationAfter(parent.login, pagination, cursor);
+          data = await fetchData(body, token);
+        }
+      } while(hasNext);
+
+      return totalCommits;
+    },
     organization: async (parent, args, { token }) => {
       const body = githubQueries.organization(args.login);
       const data = await fetchData(body, token);
