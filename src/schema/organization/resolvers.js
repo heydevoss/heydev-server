b840836fe015b3b43552cab1750359240ad5ab96
LukeHxH <lucasmed812@gmail.com>
2019-05-17 16:42:44 -0300

    Add resolvers for totalIssues in organization type

--- src/schema/organization/resolvers.js
+++ src/schema/organization/resolvers.js
@@ -1,8 +1,47 @@
 import fetchData from '../../github/dataFetcher';
 import githubQueries from '../../github/queries';
 
+const sumIssues = (issues) => {
+  let sum = 0;
+
+  issues.forEach(issue => {
+    sum += issue.issues.totalCount;
+  });
+
+  return sum;
+}
+
 export default {
   Query: {
+    issues: async (parent, args, { token }) => {
+      const pagination = 80;
+
+      let body = githubQueries.totalIssuesOrganization(parent.login, pagination);
+      let data = await fetchData(body, token);
+
+      let hasNext = true;
+
+      let totalIssues = 0;
+
+      do {
+        let repositories = data.data.repositoryOwner.repositories;
+
+        const issues = repositories.nodes;
+        const pageInfo = repositories.pageInfo;
+        const cursor = pageInfo.endCursor;
+
+        hasNext = pageInfo.hasNextPage;
+
+        totalIssues += sumIssues(issues);
+
+        if (hasNext) {
+          body = githubQueries.totalIssuesOrganizationAfter(parent.login, pagination, cursor);
+          data = await fetchData(body, token);
+        }
+      } while(hasNext);
+
+      return totalIssues;
+    },
     organization: async (parent, args, { token }) => {
       const body = githubQueries.organization(args.login);
       const data = await fetchData(body, token);
