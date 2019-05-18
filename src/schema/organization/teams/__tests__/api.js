4a6ce001b86ff9030683a2ba81f3f263554b98e9
Fanny <fanny.vieira@ccc.ufcg.edu.br>
2019-05-18 13:57:38 -0300

    Finish api for teams with documentation

---
+++ src/schema/organization/teams/__tests__/api.js
@@ -0,0 +1,79 @@
+import gql from 'graphql-tag'
+// TODO: test for two or more oganizations.
+
+/**
+ * Query for test the obtaining of all team atributtes
+ *
+ * @param {int} maxNumberOfTeams max number of results from this query
+ * @returns {TeamObject} an object containing:
+ *                   `id`
+ *                   `name`
+ *                   `slug`
+ *                   `url`
+ *                   `totalMembers`
+ *                   `repoLogin`
+ *                   the desired `organization`
+**/
+const teams = gql`
+teams(maxNumberOfTeams: $maxNumberOfTeams){
+    id,
+    name,
+    slug,
+    url,
+    totalMembers,
+    repoLogin
+}
+`
+
+/**
+ * Query for test the obtaining of one team atributte,
+ * in this case, the name.
+ *
+ * @param {int} maxNumberOfTeams max number of results from this query
+ * @returns {TeamObject} an object containing:
+ *                   `name`
+ *                   the desired `organization`
+**/
+const teamsName = gql`
+teams(maxNumberOfTeams: $maxNumberOfTeams){
+    name
+}
+`
+
+/**
+ * Query for test the obtaining of two team atributtes
+ *
+ * @param {int} maxNumberOfTeams max number of results from this query
+ * @returns {TeamObject} an object containing:
+ *                   `id`
+ *                   `url`
+ *                   the desired `organization`
+**/
+const teamsIdUrl = gql`
+teams(maxNumberOfTeams: $maxNumberOfTeams){
+    name
+}
+`
+
+/**
+ * Query for test the obtaining of three team atributtes
+ *
+ * @param {int} maxNumberOfTeams max number of results from this query
+ * @returns {TeamObject} an object containing:
+ *                   `slug`
+ *                   `totalMembers`
+ *                   `repoLogin`
+ *                   the desired `organization`
+**/
+const teamsSlugTotalMembersRepoLogin = gql`
+teams(maxNumberOfTeams: $maxNumberOfTeams){
+    name
+}
+`
+
+export {
+    teams,
+    teamsName,
+    teamsIdUrl,
+    teamsSlugTotalMembersRepoLogin
+}
