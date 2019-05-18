a618b41279800473655992f6e3c4e16ac848aa29
Fanny <fanny.vieira@ccc.ufcg.edu.br>
2019-05-18 15:55:09 -0300

    Update file including documentation

--- src/schema/user/__tests__/api.js
+++ src/schema/user/__tests__/api.js
@@ -1,5 +1,15 @@
 import gql from 'graphql-tag'
-const me = gql`
+
+/**
+ * Query for test the obtaining of all user atributtes
+ *
+ * @returns {UserObject} an object containing:
+ *                   `id`
+ *                   `login`
+ *                   `totalRepos`
+ *                   of the token passed
+**/
+const user = gql`
 query {
     me {
         id
@@ -9,4 +19,40 @@ query {
 }
 `
 
-export default me
+/**
+ * Query for test the obtaining of one user atributter
+ *
+ * @returns {UserObject} an object containing:
+ *                   `login`
+ *                   of the token passed
+**/
+const userLogin = gql`
+query {
+    me {
+        login
+    }
+}
+`
+
+/**
+ * Query for test the obtaining of two user atributtes
+ *
+ * @returns {UserObject} an object containing:
+ *                   `login`
+ *                   `totalRepos`
+ *                   of the token passed
+**/
+const userLoginTotalRepos = gql`
+query {
+    me {
+        login,
+        totalRepos
+    }
+}
+`
+
+export {
+    user,
+    userLogin,
+    userLoginTotalRepos
+}
--- src/schema/user/__tests__/user.spec.js
+++ src/schema/user/__tests__/user.spec.js
@@ -1,4 +1,4 @@
-import me from './api';
+import { user } from './api';
 import expectedResponse from './expectedResults'
 import testServer from '../../../testUtils/integration/serverFactory'
 import { createTestClient } from 'apollo-server-testing'
@@ -6,7 +6,7 @@ import { createTestClient } from 'apollo-server-testing'
 describe('User type tests', () => {
   it('me: User', async () => {
     const { query } = createTestClient(testServer);
-    const response = await query({ query: me });
+    const response = await query({ query: user });
     expect(response.data).toEqual(expectedResponse)
   });
 });
