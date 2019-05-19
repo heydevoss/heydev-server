c696d9dd65bf2e7bda79b3d2e65657fc31b99338
Fanny <fanny.vieira@ccc.ufcg.edu.br>
2019-05-19 11:14:39 -0300

    Fix conflicts

---
+++ src/schema/organization/members/__tests__/api.js
@@ -0,0 +1,100 @@
+import gql from 'graphql-tag';
+// TODO: test for two or more oganizations.
+
+/**
+ * Query for test the obtaining of all team atributtes
+ *
+ * @param {int} maxNumberOfMembers max number of results from this query
+ * @returns {TeamMemberObject} an object containing:
+ *                   `id`
+ *                   `name`
+ *                   `login`
+ *                   `role`
+ *                   `url`
+ *                   of the desired `organization`
+ * */
+const members = gql`
+members(maxNumberOfMembers: $maxNumberOfMembers){
+    id,
+    name,
+    login,
+    role,
+    url,
+}
+`;
+
+/**
+ * Query for test the obtaining of one team atributte
+ *
+ * @param {int} maxNumberOfMembers max number of results from this query
+ * @returns {TeamMemberObject} an object containing:
+ *                   `name`
+ *                   of the desired `organization`
+ * */
+const membersName = gql`
+members(maxNumberOfMembers: $maxNumberOfMembers){
+    name,
+}
+`;
+
+/**
+ * Query for test the obtaining of two team atributtes
+ *
+ * @param {int} maxNumberOfMembers max number of results from this query
+ * @returns {TeamMemberObject} an object containing:
+ *                   `id`
+ *                   `login`
+ *                   of the desired `organization`
+ * */
+const membersIdLogin = gql`
+members(maxNumberOfMembers: $maxNumberOfMembers){
+    id,
+    login
+}
+`;
+
+/**
+ * Query for test the obtaining of three team atributtes
+ *
+ * @param {int} maxNumberOfMembers max number of results from this query
+ * @returns {TeamMemberObject} an object containing:
+ *                   `login`
+ *                   `role`
+ *                   `url`
+ *                   of the desired `organization`
+ * */
+const membersLoginRoleUrl = gql`
+members(maxNumberOfMembers: $maxNumberOfMembers){
+    role,
+    login,
+    url
+}
+`;
+
+/**
+ * Query for test the obtaining of four team atributtes
+ *
+ * @param {int} maxNumberOfMembers max number of results from this query
+ * @returns {TeamMemberObject} an object containing:
+ *                   `name`
+ *                   `login`
+ *                   `role`
+ *                   `url`
+ *                   of the desired `organization`
+ * */
+const membersNameLoginRoleUrl = gql`
+members(maxNumberOfMembers: $maxNumberOfMembers){
+    name,
+    login,
+    role,
+    url
+}
+`;
+
+export {
+    members,
+    membersName,
+    membersIdLogin,
+    membersLoginRoleUrl,
+    membersNameLoginRoleUrl
+};
