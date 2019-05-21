c5ea8a2a5836d16d7e8763aa8b80c9cb0ab26c4a
LukeHxH <lucasmed812@gmail.com>
2019-05-21 14:50:53 -0300

    Add tests for organization type

--- src/schema/organization/__tests__/expectedResults.js
+++ src/schema/organization/__tests__/expectedResults.js
@@ -1,3 +1,70 @@
+const organizationData =
+{
+  organization: {
+    id: "MDEyOk9yZ2FuaXphdGlvbjQ5MTI5MTEx",
+    name: "panelinhadees",
+    login: "panelinhadees",
+    avatarUrl: "https://avatars0.githubusercontent.com/u/49129111?v=4",
+    url: "https://github.com/panelinhadees",
+    websiteUrl: null,
+    totalMembers: 10,
+    totalRepos: 4,
+    totalTeams: 4,
+    totalPullRequests: 55,
+    totalCommits: 253,
+    totalIssues: 84,
+  }
+}
+
+const organizationId =
+{
+  organization: {
+    id: "MDEyOk9yZ2FuaXphdGlvbjQ5MTI5MTEx",
+  }
+}
+
+const organizationName =
+{
+  organization: {
+    name: "panelinhadees",
+  }
+}
+
+const organizationLogin =
+{
+  organization: {
+    login: "panelinhadees",
+  }
+}
+
+const organizationWebsiteUrl =
+{
+  organization: {
+    websiteUrl: null,
+  }
+}
+
+const organizationTotalMembers =
+{
+  organization: {
+    totalMembers: 10,
+  }
+}
+
+const organizationTotalTeams =
+{
+  organization: {
+    totalTeams: 4,
+  }
+}
+
+const organizationTotalRepos =
+{
+  organization: {
+    totalRepos: 4,
+  }
+}
+
 const organizationTotalPullRequests = {
   organization: {
     totalPullRequests: 55,
@@ -6,17 +73,25 @@ const organizationTotalPullRequests = {
 
 const organizationTotalIssues = {
   organization: {
-    totalIssues: 83,
+    totalIssues: 84,
   }
 };
 
 const organizationTotalCommits = {
   organization: {
-    totalCommits: 227,
+    totalCommits: 253,
   }
 };
 
 export {
+  organizationData,
+  organizationId,
+  organizationLogin,
+  organizationName,
+  organizationWebsiteUrl,
+  organizationTotalTeams,
+  organizationTotalRepos,
+  organizationTotalMembers,
   organizationTotalPullRequests,
   organizationTotalIssues,
   organizationTotalCommits,
--- src/schema/organization/__tests__/organization.spec.js
+++ src/schema/organization/__tests__/organization.spec.js
@@ -7,6 +7,70 @@ describe('Organization type tests', () => {
   const login = 'panelinhadees';
   const { query } = createTestClient(testServer);
 
+  it('organization: Organization', async () => {
+    const { data } = await(query({
+      query: queries.organizationData,
+      variables: { login },
+    }));
+    expect(data).toEqual(expectedResults.organizationData);
+  });
+
+  it('organization: { id }: Organization', async () => {
+    const { data } = await(query({
+      query: queries.organizationId,
+      variables: { login },
+    }));
+    expect(data).toEqual(expectedResults.organizationId);
+  });
+
+  it('organization: { login }: Organization', async () => {
+    const { data } = await(query({
+      query: queries.organizationLogin,
+      variables: { login },
+    }));
+    expect(data).toEqual(expectedResults.organizationLogin);
+  });
+
+  it('organization: { name }: Organization', async () => {
+    const { data } = await(query({
+      query: queries.organizationName,
+      variables: { login },
+    }));
+    expect(data).toEqual(expectedResults.organizationName);
+  });
+
+  it('organization: { websiteUrl }: Organization', async () => {
+    const { data } = await(query({
+      query: queries.organizationWebsiteUrl,
+      variables: { login },
+    }));
+    expect(data).toEqual(expectedResults.organizationWebsiteUrl);
+  });
+
+  it('organization: { totalMembers }: Organization', async () => {
+    const { data } = await(query({
+      query: queries.organizationTotalMembers,
+      variables: { login },
+    }));
+    expect(data).toEqual(expectedResults.organizationTotalMembers);
+  });
+
+  it('organization: { totalRepos }: Organization', async () => {
+    const { data } = await(query({
+      query: queries.organizationTotalRepos,
+      variables: { login },
+    }));
+    expect(data).toEqual(expectedResults.organizationTotalRepos);
+  });
+
+  it('organization: { totalTeams }: Organization', async () => {
+    const { data } = await(query({
+      query: queries.organizationTotalTeams,
+      variables: { login },
+    }));
+    expect(data).toEqual(expectedResults.organizationTotalTeams);
+  });
+
   it('organization: { totalPullRequests }: Organization', async () => {
     const { data } = await(query({
       query: queries.organizationTotalPullRequests,
