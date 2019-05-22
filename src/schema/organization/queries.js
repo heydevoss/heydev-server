3a2d60f4caabccc1425793bd1e2dc0b3bf3f458f
LukeHxH <lucasmed812@gmail.com>
2019-05-22 19:57:44 -0300

    Change query for not specting anymore login parameter

--- src/schema/organization/queries.js
+++ src/schema/organization/queries.js
@@ -1,16 +1,8 @@
-import { GraphQLString, GraphQLNonNull } from 'graphql';
-
 import OrganizationType from './types';
 import resolvers from './resolvers';
 
 const organization = {
   type: OrganizationType,
-  args: {
-    login: {
-      type: new GraphQLNonNull(GraphQLString),
-      description: 'Organization login on Github',
-    },
-  },
   resolve: resolvers.Query.organization,
 };
 
