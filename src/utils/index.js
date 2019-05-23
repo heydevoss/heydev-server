e165220cffd22497896f2d1a464979f8c615b76e
Jadson Luan <jadsonluan1@gmail.com>
2019-05-23 16:45:21 -0300

    Remove unused class

--- src/utils/index.js
+++ src/utils/index.js
@@ -16,25 +16,3 @@ export const getOldestDate = (dates) => {
   const result = orderedDates[0];
   return new Date(result.getFullYear(), result.getMonth(), result.getDate());
 }
-
-/**
- * @class A variant of Set, which the difference is on the equality condition of objects (prop: login)
- */
-export class GeneralSet {
-  constructor() {
-      this.map = new Map();
-      this[Symbol.iterator] = this.values;
-  }
-
-  add(item) {
-    this.map.set(item.login, item);
-  }
-
-  values() {
-      return this.map.values();
-  }
-
-  delete(item) {
-      return this.map.delete(item.login);
-  }
-}
