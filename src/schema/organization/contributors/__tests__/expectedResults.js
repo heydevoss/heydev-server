2ac8048b4c70474129f76646646f3b1f6b1d4a3a
Jadson Luan <jadsonluan1@gmail.com>
2019-05-22 21:11:01 -0300

    Standardizes json

--- src/schema/organization/contributors/__tests__/expectedResults.js
+++ src/schema/organization/contributors/__tests__/expectedResults.js
@@ -1,45 +1,45 @@
 const contributors = {
-    "organization": {
-        "contributors": [
+    organization: {
+        contributors: [
             {
-                "id": "MDQ6VXNlcjY4Njc5NTg=",
-                "login": "thayannevls",
-                "name": "Thayanne Luiza",
-                "email": "thayannevls@gmail.com",
-                "websiteUrl": "https://thayannevls.github.io",
-                "location": "Campina Grande - PB",
-                "bio": "Coding for a better self and a better world.",
-                "firstContributionDate": "2019-04-27"
+                id: "MDQ6VXNlcjY4Njc5NTg=",
+                login: "thayannevls",
+                name: "Thayanne Luiza",
+                email: "thayannevls@gmail.com",
+                websiteUrl: "https://thayannevls.github.io",
+                location: "Campina Grande - PB",
+                bio: "Coding for a better self and a better world.",
+                firstContributionDate: "2019-04-27"
             },
             {
-                "id": "MDQ6VXNlcjEzNDg4MDI1",
-                "login": "jadsonluan",
-                "name": "Jadson Luan",
-                "email": "jadsonluan1@gmail.com",
-                "websiteUrl": "",
-                "location": "Campina Grande",
-                "bio": "A Computer Science undergraduate student.",
-                "firstContributionDate": "2019-04-27"
+                id: "MDQ6VXNlcjEzNDg4MDI1",
+                login: "jadsonluan",
+                name: "Jadson Luan",
+                email: "jadsonluan1@gmail.com",
+                websiteUrl: "",
+                location: "Campina Grande",
+                bio: "A Computer Science undergraduate student.",
+                firstContributionDate: "2019-04-27"
             },
             {
-                "id": "MDQ6VXNlcjE0MTEzNDgw",
-                "login": "fanny",
-                "name": "Fanny",
-                "email": "fanny.vieira@ccc.ufcg.edu.br",
-                "websiteUrl": "https://fannyvieira.github.io/portfolio/",
-                "location": "Campina Grande, PB, Brazil.",
-                "bio": "",
-                "firstContributionDate": "2019-04-26"
+                id: "MDQ6VXNlcjE0MTEzNDgw",
+                login: "fanny",
+                name: "Fanny",
+                email: "fanny.vieira@ccc.ufcg.edu.br",
+                websiteUrl: "https://fannyvieira.github.io/portfolio/",
+                location: "Campina Grande, PB, Brazil.",
+                bio: "",
+                firstContributionDate: "2019-04-26"
             },
             {
-                "id": "MDQ6VXNlcjE0MTMwNjA5",
-                "login": "RaylaMedeiros",
-                "name": "Rayla Medeiros",
-                "email": "rayla.araujo@ccc.ufcg.edu.br",
-                "websiteUrl": "https://github.com/RaylaMedeiros",
-                "location": "Campina Grande, PB, Brazil",
-                "bio": "",
-                "firstContributionDate": "2019-05-01"
+                id: "MDQ6VXNlcjE0MTMwNjA5",
+                login: "RaylaMedeiros",
+                name: "Rayla Medeiros",
+                email: "rayla.araujo@ccc.ufcg.edu.br",
+                websiteUrl: "https://github.com/RaylaMedeiros",
+                location: "Campina Grande, PB, Brazil",
+                bio: "",
+                firstContributionDate: "2019-05-01"
             }
         ]
     }
